7d6da42c5cf8672736e90371a96870ab66492c9b
cheryldp <cheryldp@192.168.0.4>
2019-08-27 17:22:19 -0400

    First Trial of Novice Program

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -9,6 +9,21 @@ public class A1Adept {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int count = scan.nextInt();
+		
+		for (i = 0, i < count, i++);
+				// read all items, assign variable and price
+				String food = scan.next();
+				int price = scan.nextInt();
+				if food = scan.next(); {
+					int cus = price * scan.nextInt();
+					else { String food = scan.next();
+				}
+				// scan for next string, assign to variable, scan for variable
+				// multiply by that variable price per customer
+		System.out.println("Biggest: " + big);
+		System.out.printIn("Smallest: " + small);
+		System.out.printIn("Average: " + avg);
 		
 	}
 }
--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,24 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int customers = scan.nextInt();
+		for (int trials = 0; trials <= customers; trials++) {
+			String names = scan.next();
+			String F = names.charAt(0);
 		
+			String Last = scan.next();
+		
+			int count = scan.nextInt();
+				for (int i = 0; i <= count; i++ ) {
+					int quantity = scan.nextInt();
+					int price = scan.nextInt();
+					int itemTotal = price * quantity;
+					int Total = itemTotal;
+		}
+		
+	
+		
+		System.out.println(F.  "+ " Last.  "+ " Total );
+		}
 	}
 }
