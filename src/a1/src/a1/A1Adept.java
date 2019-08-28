34ba12f0661df1b6e1048ab8291bc16b07c85ca4
cheryldp <cheryldp@192.168.0.4>
2019-08-28 15:02:46 -0400

    Syntax

--- src/a1/src/a1/A1Adept.java
+++ src/a1/src/a1/A1Adept.java
@@ -15,16 +15,16 @@ public class A1Adept {
 				// read all items, assign variable and price
 				String food = scan.next();
 				int price = scan.nextInt();
-				if food = scan.next(); {
-					int cus = price * scan.nextInt();
-					else { String food = scan.next();
+				if ( food.equals(scan.next())) {
+					int cus = price * scan.nextInt(); }
+					else { food = scan.next();
 				}
 				}
 				// scan for next string, assign to variable, scan for variable
 				// multiply by that variable price per customer
-		System.out.println("Biggest: " + big);
-		System.out.printIn("Smallest: " + small);
-		System.out.printIn("Average: " + avg);
+		// System.out.println("Biggest: " + big);
+		// System.out.printIn("Smallest: " + small);
+	   //System.out.printIn("Average: " + avg);
 		
 	}
 }
